c6dfab26c2eef66659d6404ea9b0fae4b7ed0c22
h5c52 <vincent-charles.lavoie.1@ens.etsmtl.ca>
2025-11-05 23:52:48 -0500

    perfectionification du test.py

--- test.py
+++ test.py
@@ -1,25 +1,32 @@
-import sympy as sp
-import numpy as np
+from sympy import *
 
-theta2, theta3, theta4, d1 = sp.symbols('theta2, theta3, theta4, d1')
+theta2, theta3, theta4, d1, nx, ox, ax, px, ny, oy, ay, py, nz, oz, az, pz = symbols('theta2, theta3, theta4, d1, n_x, o_x, a_x, p_x, n_y, o_y, a_y, p_y, n_z, o_z, a_z, p_z')
+
+# s,c act as aliases to sin,cos
+s = sin
+c = cos
+
+# the intended short-hand functions used to replace sin/cos later on
+s1 = Function('s')
+c1 = Function('c')
 
 def dh_matrix(theta, d, a, alpha):
-    ct = sp.cos(theta)
-    st = sp.sin(theta)
-    ca = sp.cos(alpha)
-    sa = sp.sin(alpha)
+    ct = cos(theta)
+    st = sin(theta)
+    ca = cos(alpha)
+    sa = sin(alpha)
 
-    return sp.Matrix([
+    return Matrix([
         [ct, -st*ca, st*sa, a*ct],
         [st, ct*ca, -ct*sa, a*st],
         [0, sa, ca, d],
         [0, 0, 0, 1]
     ])
 
-H10 = dh_matrix(0, 215+d1, 0, 0)
-H21 = dh_matrix(theta2, 0, 800, 90)
-H32 = dh_matrix(theta3+90, 0, 0, 90)
-H43 = dh_matrix(theta4, 400, 0, 0)
+H1 = dh_matrix(0, 215+d1, 0, 0)
+H2 = dh_matrix(theta2, 0, 800, pi/2)
+H3 = dh_matrix(theta3+(pi/2), 0, 0, pi/2)
+H4 = dh_matrix(theta4, 400, 0, 0)
 
 #print("H10:")
 #sp.pprint(H10);
@@ -30,14 +37,14 @@ H43 = dh_matrix(theta4, 400, 0, 0)
 #print("H43:")
 #sp.pprint(H43);
 
-H0R = sp.Matrix([
+H0R = Matrix([
     [0, -1, 0, 0],
     [1, 0, 0, -500],
     [0, 0, 1, 0],
     [0, 0, 0, 1]
     ])
 
-Ho4 =  sp.Matrix([
+Ho4 = Matrix([
     [1, 0, 0, 0],
     [0, 1, 0, 0],
     [0, 0, 1, 100],
@@ -49,16 +56,29 @@ Ho4 =  sp.Matrix([
 #print("Ho4:")
 #sp.pprint(Ho4);
 
-H40 = H10 * H21 * H32 * H43
+H40 = H1 * H2 * H3 * H4
 H = H0R * H40 * Ho4
 
 #print("H:")
 #sp.pprint(H);
 
-H_outil_R = sp.Matrix([
-    [cos(ez)*cos(ey), cos(ez)*sin(ey)*sin(ex)-sin(ez)*cos(ex), cos(ez)*sin(ey)*cos(ex)+sin(ez)*sin(ex),x],
-    [sin(ez)*cos(ey), sin(ez)*sin(ey)*sin(ex)+cos(ez)*cos(ex), sin(ez)*sin(ey)*cos(ex)-cos(ez)*sin(ex), y],
-    [-sin(ey), cos(ey)*sin(ex), cos(ey)*cos(ex), z],
+#H_outil_R = sp.Matrix([
+#    [cos(ez)*cos(ey), cos(ez)*sin(ey)*sin(ex)-sin(ez)*cos(ex), cos(ez)*sin(ey)*cos(ex)+sin(ez)*sin(ex),x],
+#    [sin(ez)*cos(ey), sin(ez)*sin(ey)*sin(ex)+cos(ez)*cos(ex), sin(ez)*sin(ey)*cos(ex)-cos(ez)*sin(ex), y],
+#    [-sin(ey), cos(ey)*sin(ex), cos(ey)*cos(ex), z],
+#    [0, 0, 0, 1]])
+
+P = Matrix([
+    [nx, px, ax, px],
+    [ny, oy, ay, py],
+    [nz, oz, az, pz],
     [0, 0, 0, 1]])
 
-a1 = h10^-1
+eq1 = simplify(H4)
+eq2 = simplify((H1*H2*H3).inv()*P)
+eq = Eq(eq1, eq2)
+
+eq_latex = str(latex(eq)).replace('\sin{', 's_{').replace('\cos{','c_{').replace('\sin^{2}{','s^2_{')
+print(eq_latex)
+
+#pprint(eq.replace(s,s1).replace(c,c1))
