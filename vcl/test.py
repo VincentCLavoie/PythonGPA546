017852d8dcb8f8c9f1acef25676aa62a2d1c863e
h5c52 <vincent-charles.lavoie.1@ens.etsmtl.ca>
2025-11-11 12:36:11 -0500

    test.py done

--- vcl/test.py
+++ vcl/test.py
@@ -1,15 +1,16 @@
 from sympy import *
+import pyperclip
 
+# Symboles
 theta2, theta3, theta4, d1, nx, ox, ax, px, ny, oy, ay, py, nz, oz, az, pz = symbols('theta2, theta3, theta4, d1, n_x, o_x, a_x, p_x, n_y, o_y, a_y, p_y, n_z, o_z, a_z, p_z')
 
-# s,c act as aliases to sin,cos
+# Aliases pour réduire la grosseur des résultats
 s = sin
 c = cos
-
-# the intended short-hand functions used to replace sin/cos later on
 s1 = Function('s')
 c1 = Function('c')
 
+# Fonction qui retourne une matrice de transformation selon des paramètres DH
 def dh_matrix(theta, d, a, alpha):
     ct = cos(theta)
     st = sin(theta)
@@ -23,20 +24,13 @@ def dh_matrix(theta, d, a, alpha):
         [0, 0, 0, 1]
     ])
 
+# Liste des matrices DH
 H1 = dh_matrix(0, 215+d1, 0, 0)
 H2 = dh_matrix(theta2, 0, 800, pi/2)
 H3 = dh_matrix(theta3+(pi/2), 0, 0, pi/2)
 H4 = dh_matrix(theta4, 400, 0, 0)
 
-#print("H10:")
-#sp.pprint(H10);
-#print("H21:")
-#sp.pprint(H21);
-#print("H32:")
-#sp.pprint(H32);
-#print("H43:")
-#sp.pprint(H43);
-
+# Matrice de transformation H0, atelier
 H0R = Matrix([
     [0, -1, 0, 0],
     [1, 0, 0, -500],
@@ -44,6 +38,7 @@ H0R = Matrix([
     [0, 0, 0, 1]
     ])
 
+# Matrice de transformation Houtil, H4
 Ho4 = Matrix([
     [1, 0, 0, 0],
     [0, 1, 0, 0],
@@ -51,34 +46,29 @@ Ho4 = Matrix([
     [0, 0, 0, 1]
     ])
 
-#print("H0R:")
-#sp.pprint(H0R);
-#print("Ho4:")
-#sp.pprint(Ho4);
-
-H40 = H1 * H2 * H3 * H4
-H = H0R * H40 * Ho4
-
-#print("H:")
-#sp.pprint(H);
-
-#H_outil_R = sp.Matrix([
-#    [cos(ez)*cos(ey), cos(ez)*sin(ey)*sin(ex)-sin(ez)*cos(ex), cos(ez)*sin(ey)*cos(ex)+sin(ez)*sin(ex),x],
-#    [sin(ez)*cos(ey), sin(ez)*sin(ey)*sin(ex)+cos(ez)*cos(ex), sin(ez)*sin(ey)*cos(ex)-cos(ez)*sin(ex), y],
-#    [-sin(ey), cos(ey)*sin(ex), cos(ey)*cos(ex), z],
-#    [0, 0, 0, 1]])
-
+# Matrice P 
 P = Matrix([
     [nx, px, ax, px],
     [ny, oy, ay, py],
     [nz, oz, az, pz],
     [0, 0, 0, 1]])
 
-eq1 = simplify(H4)
-eq2 = simplify((H1*H2*H3).inv()*P)
-eq = Eq(eq1, eq2)
+############################################
+# Configuration de l'égalité
+# 
+# Exemple: eq1 = H1*H2*H3, eq2 = P*H4.inv()
 
-eq_latex = str(latex(eq)).replace('\sin{', 's_{').replace('\cos{','c_{').replace('\sin^{2}{','s^2_{')
-print(eq_latex)
+eq1 = H1*H2*H3
+eq2 = P*H4.inv()
+############################################
 
-#pprint(eq.replace(s,s1).replace(c,c1))
+# Création de l'équalité
+eq = Eq(simplify(eq1), simplify(eq2))
+
+# Transforme la réponse en code LaTeX et la copy sur le presse-papier
+eq_latex = str(latex(eq)).replace('\sin{', 's_{').replace('\cos{','c_{').replace('\sin^{2}{','s^2_{').replace('\cos^{2}{','c^2_{')
+print("latex version printed to clipboard")
+pyperclip.copy(eq_latex)
+
+# Imprime le résultat
+pprint(eq.replace(s,s1).replace(c,c1))
